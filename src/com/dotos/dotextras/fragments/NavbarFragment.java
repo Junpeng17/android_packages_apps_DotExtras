b469a10ba04549942c76ca29eb74e4494c264872
ganeshi4u <ganeshvarmai4u@gmail.com>
2017-11-05 18:32:10 +0530

    add trigger to open CM Navbar settings
    Signed-off-by: ganeshi4u <ganeshvarmai4u@gmail.com>

--- src/com/dotos/dotextras/fragments/NavbarFragment.java
+++ src/com/dotos/dotextras/fragments/NavbarFragment.java
@@ -1,13 +1,15 @@
 package com.dotos.dotextras.fragments;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.content.Intent;
+import android.content.Context;
 
 import com.dotos.R;
 
-
-public class NavbarFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+public class NavbarFragment extends PreferenceFragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -15,10 +17,5 @@ public class NavbarFragment extends PreferenceFragment implements Preference.OnP
 
         addPreferencesFromResource(R.xml.dot_navbar);
 
-    }
-
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object o) {
-        return false;
     }
 }
